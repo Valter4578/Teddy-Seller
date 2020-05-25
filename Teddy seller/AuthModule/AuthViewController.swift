6925c1f7f6c9e3279627c597934bc23a3e676619
Максим Алексеев <mrvalter566@gmail.com>
2020-05-25 13:46:49 +0700

    Add ios 11 support

---
+++ Teddy seller/AuthModule/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  Teddy seller
+//
+//  Created by Максим Алексеев on 25.05.2020.
+//  Copyright © 2020 Максим Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
