f37e18ab7bcac9aa32d23a23ba3026d2ba0052e5
Максим Алексеев <mrvalter566@gmail.com>
2020-05-25 14:13:50 +0700

    Create draft of auth view controller

--- Teddy seller/AuthModule/AuthViewController.swift
+++ Teddy seller/AuthModule/AuthViewController.swift
@@ -7,24 +7,83 @@
 //
 
 import UIKit
+import SnapKit
 
 class AuthViewController: UIViewController {
+    // MARK:- Views
+    var teddyImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Teddy")
+        return imageView
+    }()
 
+    var phoneTextField: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 16
+        textField.backgroundColor = .white
+        
+        textField.attributedPlaceholder = NSAttributedString(string: "+7", attributes: [NSAttributedString.Key.foregroundColor: UIColor.placeholderBlack, NSAttributedString.Key.font: UIFont(name: "Helvetica Neue", size: 24)])
+        
+        return textField
+    }()
+    
+    var label: UILabel = {
+        let label = UILabel()
+        label.text = "Введите номер телефона"
+        label.font = UIFont(name: "Helvetica Neue", size: 24)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var nextButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .authNextBlue
+        button.titleLabel?.text = "Далее"
+        button.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 36)
+        button.titleLabel?.textColor = .authNextGray
+        return button
+    }()
+    
+    // MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .mainBlue
+        
+        setupNextButton()
+        setupTeddyImageView()
+        setupLabel()
+        setupTextField()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK:- Setups
+    private func setupNextButton() {
+        view.addSubview(nextButton)
+        
+        nextButton.snp.makeConstraints { (maker) in
+        }
     }
-    */
+    
+    private func setupTeddyImageView() {
+        view.addSubview(teddyImageView)
+        
+        teddyImageView.snp.makeConstraints { (maker) in
 
+        }
+    }
+    
+    private func setupLabel() {
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { (maker) in
+        }
+    }
+    
+    private func setupTextField() {
+        view.addSubview(phoneTextField)
+        
+        phoneTextField.snp.makeConstraints { (maker) in
+        }
+    }
 }
+
