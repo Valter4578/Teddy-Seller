f8b4faf7d3d85c0cb1b0b722946c40afca275ad4
Максим Алексеев <mrvalter566@gmail.com>
2020-06-10 17:46:18 +0700

    Added AddAd alert builder

---
+++ Teddy seller/CoreLayer/Builders/AddAdAlertBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  AddAdAlertBuilder.swift
+//  Teddy seller
+//
+//  Created by Максим Алексеев on 10.06.2020.
+//  Copyright © 2020 Максим Алексеев. All rights reserved.
+//
+
+import Foundation
